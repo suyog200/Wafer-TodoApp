0031286f1ffdf9cfdd767346ba55f96b0d519161
suyog chari <suygchari@gmail.com>
2025-09-21 17:08:25 +0530

    configuration fixes

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,10 +1,8 @@
 import mongoose from  "mongoose";
 
 const connectDB = async () => {
-    console.log(process.env.MONGODB_URI);
     try {
         const conn = await mongoose.connect(process.env.MONGODB_URI || "");
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(error);
         process.exit(1);
